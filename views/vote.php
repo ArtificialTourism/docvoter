f17a8a54ce348f00f02b13d904209bef13531190
Alicia Comella <malicias@gmail.com>
2014-01-10 22:17:31 +0000

    vote counter added

--- views/vote.php
+++ views/vote.php
@@ -1,16 +1,20 @@
 <?php //var_dump($event); ?>
 <div class="container_4">
-	<div id="page-heading" class="clearfix">
+	<div id="page-heading">
 	    <div class="grid-wrap">
-			<div class="grid_1 align_right add-driver float_right">
-    			    <!-- <a href="index.php?do=card&event=<?php echo $event->id?>" class="button blue large">+ add driver</a> -->
-    		</div>
-    		<div class="grid_3">
-    		    <?php if (isset($event->summary)&&$event->summary!=''){?>
-    		    <h2><?php echo($event->summary);?></h2>
-    		    <?php }?>
+            <div class="grid_4">
+            <?php if (isset($event->summary)&&$event->summary!=''){?>
+                <h2><?php echo($event->summary);?></h2>
+                <?php }?>
+            </div>
+         </div>
+    </div>
+    <div class="grid-wrap clearfix">
+    		<div class="grid_2">
     		       <ul id="category-nav">
-    		           <li><a id="top50" class="active" href=""><?php if (count($top50) < 50) { echo('view all'); } else { echo('top '.count($top50)); } ?></a></li><?php $cols = array('67403a','6b8f66','e82c2a','ec248e','d8a50e','339999','ff5e10','0c8b32','b0126f','062b60','a17317','53b9be','adadad','f83f5f');
+    		          <?php if ($event->id!=64) { ?>
+                           <li><a id="top50" class="active" href=""><?php if (count($top50) < 50) { echo('view all'); } else { echo('top '.count($top50)); } ?></a></li>
+                       <?php } ?><?php $cols = array('b0126f','339999','ff5e10','0c8b32','b0126f','062b60','a17317','53b9be','adadad','f83f5f');
     		          $count=0;
     		           foreach($collection['categories'] as $cat_id=>$category){
     		               $clean_cat = dirify($category);
@@ -26,12 +30,17 @@
                        } unset($count); unset($style); ?>
     		       </ul>
     		</div>
-    		
+            <div class="grid_2">
+                <div class="counters">
+                    <!-- <a href="index.php?do=card&event=<?php echo $event->id?>" class="button blue large">+ add driver</a> -->
+            <span class="counter">section votes <span id="section_votes" class="curr_count black">0</span><span class='muted'>/</span>3</span><span class="counter">total votes <span id="total_votes" class="curr_count black">0</span><span class='muted'>/</span>12</span></span>
+
+                </div>
+            </div>
 	    </div>
-    </div>
 </div>
 <div class="container_4">
-	    <div class="grid-wrap" class="clearfix">
+	    <div class="grid-wrap clearfix">
     		<div class="grid_4">
     		    <div class="panel">
     		       <?php if (count($event_cards)){?>
@@ -58,7 +67,11 @@
     		       </ul>
     		       <?php } else { echo('<h3 class="content no-cap push-down">This event has no drivers yet. <a href="index.php?do=card&event='.$event->id.'">+ add your driver here</a>.</h3>');}?>
     		     </div>
-    		     <p><?php if($data['event']->id==61){echo("Cliquez sur les onglets ci-dessus pour voir plus de moteurs dans chaque cat&eacute;gorie.");}else{echo('Click on the tabs above to see more drivers under each category.');}?></p>
+    		     <p>
+                 <?php if($data['event']->id==61){echo("Cliquez sur les onglets ci-dessus pour voir plus de moteurs dans chaque cat&eacute;gorie.");}
+                 else{echo('Click on the tabs above to vote on the drivers under each category.');}?>
+                 <?php if ($event->id==64) { ?> You can choose <b>three</b> drivers in each category.<?php }?>
+                 </p>
     		</div>
 	    </div>
 </div>
@@ -67,10 +80,19 @@
 <script src="assets/js/poshytip-1.1/src/jquery.poshytip.min.js" type="text/javascript"></script>
 <!--	Load the poshytip stylesheet. -->
 <link rel="stylesheet" href="assets/js/poshytip-1.1/src/tip-twitter/tip-twitter.css" type="text/css" media="screen" />
+<?php foreach($collection['categories'] as $cat_id=>$category){
+      $clean_cats[$cat_id] = dirify($category);}?>
 <script type="text/javascript">
 $(document).ready(function() {
 	var event_id=<?php echo $event->id;?>;
 	var owner=<?php echo $_SESSION['user']->id;?>;
+    var currsection;
+    var sections = ['<?php echo implode("','",$clean_cats); ?>'];
+    var sectionvotes = [];
+    sections.forEach(function(element) {
+        sectionvotes[element]=0;
+    });
+    var totalvotes=0;
 	//navigation
 	$('#category-nav li a').poshytip({
         content: 'click tab to see more drivers',
@@ -80,7 +102,7 @@ $(document).ready(function() {
     	alignY: 'bottom',
     	timeOnScreen:3000,
     	allowTipHover: false,
-    	offsetY: 8,	
+    	offsetY: 8,
         slide: false
     });
     $('#top50').poshytip('disable');
@@ -90,9 +112,9 @@ $(document).ready(function() {
             $('#category-nav li a').poshytip('enable');
             $(this).addClass('active');
             $(this).poshytip('disable');
-            $('#vote-cloud li').hide();   
-            var class_to_show = $(this).attr('id');
-            $('#vote-cloud li.'+class_to_show).show();
+            $('#vote-cloud li').hide();
+            currsection = $(this).attr('id');
+            $('#vote-cloud li.'+currsection).show();
         }
     });
     //tooltip
@@ -103,19 +125,28 @@ $(document).ready(function() {
     	alignX: 'center',
     	alignY: 'bottom',
     	allowTipHover: false,
-    	offsetY: 8,	
+    	offsetY: 8,
         slide: false
     });
     $(".card").click(function(){
         //voting stuff
         var currcard = $(this);
-        if(!currcard.hasClass('voted')) {
+        console.log(sectionvotes[currsection]);
+        if(!currcard.hasClass('voted')&&sectionvotes[currsection]<3) {
         	//vote
         	var query_url = "includes/callAPI.php?action=vote/post&event_id="+event_id+"&owner="+owner+"&card_id="+$(this).attr('id');
             currcard.poshytip('update', 'sending...');
             $.ajax({
 	            url: query_url,
 	            success: function(data){/*... it worked*/
+                    sectionvotes[currsection]= sectionvotes[currsection]+1;
+                    $("#section_votes").text(sectionvotes[currsection]);
+                    totalvotes = totalvotes +1;
+                    $("#total_votes").text(totalvotes);
+                    if (sectionvotes[currsection]==3){
+                        $("."+currsection+" .card").poshytip('update', 'maximum votes reached');
+                        $("."+currsection+" .card.voted").poshytip('update', 'click title to cancel');
+                    }
 	                currcard.addClass('voted');
 	                currcard.poshytip('update', 'click title to cancel');
 	            },
@@ -124,13 +155,19 @@ $(document).ready(function() {
 	                currcard.poshytip('update', 'there was an error sending vote, please try again later',true);
 	            }
 	        });
-        } else {
+        } else if(currcard.hasClass('voted')){
             //unvote
             var query_url = "includes/callAPI.php?action=vote/delete&event_id="+event_id+"&owner="+owner+"&card_id="+$(this).attr('id');
             currcard.poshytip('update', 'removing...');
         	$.ajax({
                 url: query_url,
                 success: function(data){/*... it worked*/
+                       sectionvotes[currsection]=sectionvotes[currsection]-1;
+                       $("#section_votes").text(sectionvotes[currsection]);
+                       totalvotes = totalvotes ;
+                       $("#total_votes").text(totalvotes);
+                       $("."+currsection+" .card").poshytip('update', 'click to vote');
+                       $("."+currsection+" .card.voted").poshytip('update', 'click title to cancel');
                        currcard.removeClass('voted');
                        currcard.poshytip('update', 'click to vote');
                     },
@@ -141,5 +178,13 @@ $(document).ready(function() {
             });
         }
     });
+    if (event_id==64){
+        var btn=$('#category-nav li #schools');
+        btn.addClass('active');
+        btn.poshytip('disable');
+        $('#vote-cloud li').hide();
+        currsection = 'schools';
+        $('#vote-cloud li.'+'schools').show();
+    }
 });
 </script>
